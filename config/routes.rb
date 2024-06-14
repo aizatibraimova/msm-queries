b4d32ba5c055bb2dbf33ad3fd76dd13a6ba57f45
Aizat Ibraimova <97326728+aizatibraimova@users.noreply.github.com>
2024-06-14 15:32:36 +0000

    creates routes, adds directors#index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "directors", :action => "index"})
+
+  get("/directors/eldest", { :controller => "eldest", :action => "senior" })
+
+  get("/directors/youngest", { :controller => "youngest", :action => "junior"})
+
+  get("/movies", { :controller => "movies", :action => "index" })
+
+  get("/actors", { :controller => "actors", :action => "index"})
+
+  get("/directors/:id", { :controller => "directors", :action => "show"})
+
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+
+  get("/actors/:id", { :controller => "actors", :action => "show"})
+
 end
